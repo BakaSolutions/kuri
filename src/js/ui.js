b97e649d0938bd66ddfac9bfd943382a3306c823
man90 <man90@tuta.io>
2017-08-24 12:51:20 +0200

    Add asynchronously loading posts to floating post script

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,10 +1,22 @@
 const CACHED_POSTS = {};
 
 function displayFloatingPost(link) {
-	const NUMBER = link.href.split('/').pop();
+	const BOARD = link.href.split('/').shift();
+	const NUMBER = link.href.split('#').pop();
+	let postJson;
 
 	if (!CACHED_POSTS.hasOwnProperty(NUMBER)){
-		CACHED_POSTS[NUMBER] = document.querySelector(`#wrapper-${NUMBER}`).cloneNode(true);
+		if (document.querySelector(`#wrapper-${NUMBER}`)) {
+			CACHED_POSTS[NUMBER] = document.querySelector(`#wrapper-${NUMBER}`).cloneNode(true);
+		} else {
+			let xhr = new XMLHttpRequest();
+			xhr.open('GET', `https://tuderi.tumba.ch:48596/api/post.get?boardName=${BOARD}&postNumber=${NUMBER}`);
+			xhr.onload = () => {
+				postJson = xhr.response;
+				console.log(postJson);
+			};
+			xhr.send(null);
+		}
 	}
 
 	CACHED_POSTS[NUMBER].classList.add('floating');
