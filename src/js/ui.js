c02059f0900602b00c2ab229181b7eb625e13608
man90 <man90@tuta.io>
2017-08-24 17:21:41 +0200

    Add post spoiling function

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,3 +1,9 @@
+function spoil(button, thread) {
+	const TARGET = thread ? button.parentNode.parentNode.parentNode.parentNode : button.parentNode.parentNode.parentNode;
+
+	TARGET.className.indexOf('spoiled') > -1 ? TARGET.classList.remove('spoiled') : TARGET.classList.add('spoiled');
+};
+
 const CACHED_POSTS = {};
 
 function displayFloatingPost(link) {
