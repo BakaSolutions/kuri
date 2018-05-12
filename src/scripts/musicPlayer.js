4a999fa05f16a2c676486022757b97dccd0054f1
man90 <man90@tuta.io>
2018-05-12 17:11:43 +0200

    Add primitive audio player

--- src/scripts/musicPlayer.js
+++ src/scripts/musicPlayer.js
@@ -1,6 +1,47 @@
-const pauseButton = document.querySelector("#musicPlayer #pause");
+const musicPlayer = {
+	pauseButton: sel("#musicPlayer #pause"),
+	progressBar: sel("#musicPlayer #progress"),
+	track: null
+}
 
+musicPlayer.pauseButton.addEventListener("click", () => {
+	let b = musicPlayer.pauseButton,
+			t = musicPlayer.track
 
-pauseButton.addEventListener("click", (event) => {
-	pauseButton.innerHTML = pauseButton.innerHTML == "pause" ? "play_arrow" : "pause";
+	if (b.innerHTML == "pause"){
+		b.innerHTML = "play_arrow"
+		t.pause()
+	} else{
+		b.innerHTML = "pause"
+		t.play()
+
+		musicPlayer.track.addEventListener("timeupdate", () => {
+			let t = musicPlayer.track,
+					p = musicPlayer.progressBar
+
+			p.value = t.currentTime / t.duration * 100
+		})
+
+		musicPlayer.track.addEventListener("ended", () => {
+			musicPlayer.pauseButton.innerHTML = "play_arrow"
+		})
+	}
 })
+
+musicPlayer.progressBar.addEventListener("change", () => {
+	let t = musicPlayer.track,
+			p = musicPlayer.progressBar
+
+	t.currentTime = t.duration * p.value / 100
+})
+
+function loadAudio(e, uri, trackname) {
+	e.preventDefault()
+	e.stopPropagation()
+
+	sel("#musicPlayer").removeAttribute("hidden")
+
+	sel("#musicPlayer #title").innerHTML = trackname
+	musicPlayer.track = new Audio(uri)
+	musicPlayer.pauseButton.click()
+}
