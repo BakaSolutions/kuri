dadd9959110389a4048ebd7f27af2b40ed189c2b
rngnrs <sup@tumba.ch>
2018-06-23 21:21:09 +0700

    Edit config that is untracked by git

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
+/**
+ * You can stop tracking this file by typing:
+ * git update-index --assume-unchanged config.js
+ *
+ * To undo and start tracking again:
+ * git update-index --no-assume-unchanged config.js
+*/
 // Edit this file due to app/helpers/config.js.
-
-// You can require any files for the config.
+// You can require any modules for the config.
 const URL = require('url');
 
 module.exports = {
