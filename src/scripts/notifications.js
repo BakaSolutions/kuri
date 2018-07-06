fd4c198d0e9b782e27fa793f1287d79c466c4f71
man90 <man90@tuta.io>
2018-07-06 21:56:27 +0200

    Implement manual notification removing & fix Toombalick's pawcode

--- src/scripts/notifications.js
+++ src/scripts/notifications.js
@@ -8,28 +8,38 @@ const notifications = {
 
       createElement("div", {className: parameters.class, innerHTML: parameters.text})
         .then(el => {
-          el.dataset.notificationId = id
+					el.dataset.notificationId = id
           this.container.insertBefore(el, this.container.childNodes[0])
 
           setTimeout(() => el.classList.add("newNotification"), 50);
 
-          parameters.timeout ? setTimeout(() => {
-            el.classList.remove("newNotification")
+					if (parameters.timeout){
+						setTimeout(() => {
+							notifications.remove(id);
+						}, parameters.timeout)
+					}
 
-            setTimeout(() => notifications.remove(id), 200);
-          }, parameters.timeout) : 0
+					if(!parameters.nonclosable){
+						el.onclick = () => {
+							notifications.remove(id)
+						}
+					}
         });
 
         return id
     }
   },
 
-  remove: function(id){
-    setTimeout(() => {
-      let el = sel(`[data-notification-id="${id}"]`)
+	remove: function(id){
+		setTimeout(() => {
+			let el = sel(`[data-notification-id="${id}"]`)
 
-      if (el) this.container.removeChild(el)
-      else console.log("Element doesn't exist.")
-    }, 100);
-  }
+			if (el){
+				el.classList.remove("newNotification")
+				setTimeout(() => {
+					this.container.removeChild(el)
+				}, 200)
+			} else console.log("Element doesn't exist.")
+		}, 100);
+	}
 }
