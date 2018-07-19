45c099788554076fbd9477b60f28512a9bd45fb0
man90 <man90@tuta.io>
2018-07-19 17:31:18 +0200

    Implement themes changing

--- src/scripts/themes.js
+++ src/scripts/themes.js
@@ -1,63 +1,51 @@
-// const THEMES = {
-// 	"Tumbach": {
-// 		"textcolor": "#e1efff",
-// 		"textcolor_secondary": "#c1cedd",
-// 		"linkcolor": "#79bde2",
-// 		"linkcolor_hover": "#5496b9",
-// 		"btncolor": "#141e27",
-// 		"btncolor_hover": "#18252f",
-// 		"bgcolor": "#10161f",
-// 		"bgcolor_card": "#141e27",
-// 		"bordercolor": "#0e1a22",
-// 		"shadowcolor": "#000"
-// 	},
-//
-// 	"Tumbach Light": {
-// 		"textcolor": "#000",
-// 		"textcolor_secondary": "#000",
-// 		"linkcolor": "#08F",
-// 		"linkcolor_hover": "#038",
-// 		"btncolor": "#b2e2ee",
-// 		"btncolor_hover": "#b2e2ee",
-// 		"bgcolor": "#fff",
-// 		"bgcolor_card": "#ccecf4",
-// 		"bordercolor": "#0e1a22",
-// 		"shadowcolor": "#000"
-// 	}
-// }
-//
+function initThemes (){
+	if (localStorage.theme) applyTheme(localStorage.theme)
+}
+
+async function loadTheme(uri) {
+	uri = window.location.origin + uri
+
+	console.log("Loading theme from ", uri)
+
+	let xhr = new XMLHttpRequest()
+
+	xhr.onload = (e) => {
+		if (xhr.status < 400){
+			applyTheme(xhr.response)
+		}
+	}
+	xhr.onerror = (e) => console.log("Error: ", uri, xhr, e);
+
+	xhr.open('GET', uri);
+	xhr.send(null);
+
+	// (async () => {
+	// 	localStorage.theme = JSON.stringify(cssObj);
+	// 	EDITOR.value = (() => {
+	// 		let styleString = '';
+	// 		for (let key in cssObj) styleString += `${key}: ${cssObj[key]}\n`;
+	// 		return styleString;
+	// 	})();
+    //
+	// 	return cssObj
+	// })().then(cssObj => {
+	// 	let css = ':root{';
+	// 	for (let key in cssObj) css += `--${key}:${cssObj[key]};`;
+	// 	css += '}';
+    //
+	// 	let node = sel('#theme');
+	// 	if (node) node.innerHTML = css
+	// 	else{
+	// 		createElement('style', {
+	// 			'type': 'text/css',
+	// 			'innerHTML': css,
+	// 			'id': 'theme'
+	// 		}).then(el => sel('head').appendChild(el))
+	// 	}
+	// })
+}
+
 // const EDITOR = document.querySelector('#themeEditor');
-//
-// let initThemes = () => setTheme(JSON.parse(localStorage.theme || "false") || THEMES["Tumbach"]);
-//
-// async function setTheme(cssObj) {
-// 	log('Applied theme:', cssObj);
-//
-// 	(async () => {
-// 		localStorage.theme = JSON.stringify(cssObj);
-// 		EDITOR.value = (() => {
-// 			let styleString = '';
-// 			for (let key in cssObj) styleString += `${key}: ${cssObj[key]}\n`;
-// 			return styleString;
-// 		})();
-//
-// 		return cssObj
-// 	})().then(cssObj => {
-// 		let css = ':root{';
-// 		for (let key in cssObj) css += `--${key}:${cssObj[key]};`;
-// 		css += '}';
-//
-// 		let node = sel('#theme');
-// 		if (node) node.innerHTML = css
-// 		else{
-// 			createElement('style', {
-// 				'type': 'text/css',
-// 				'innerHTML': css,
-// 				'id': 'theme'
-// 			}).then(el => sel('head').appendChild(el))
-// 		}
-// 	})
-// }
 //
 // EDITOR.onchange = async () => {
 // 	let cssObj = {};
@@ -71,5 +59,11 @@
 // 	setTheme(cssObj);
 // };
 
+function applyTheme(json) {
+	localStorage.theme = json
+	let object = JSON.parse(json)
 
-let initThemes = () => 0;
+	for (let rule in object) {
+		document.documentElement.style.setProperty(`--${rule}`, object[rule])
+	}
+}
