964b47e37d591dc9a9d30e068b81f3cca76db459
man90 <man90@tuta.io>
2018-07-19 19:51:17 +0200

    Implement theme editor

--- src/scripts/themes.js
+++ src/scripts/themes.js
@@ -3,67 +3,39 @@ function initThemes (){
 }
 
 async function loadTheme(uri) {
-	uri = window.location.origin + uri
+	if (uri) {
+		uri = window.location.origin + uri
 
-	console.log("Loading theme from ", uri)
+		console.log("Loading theme from ", uri)
 
-	let xhr = new XMLHttpRequest()
+		let xhr = new XMLHttpRequest()
 
-	xhr.onload = (e) => {
-		if (xhr.status < 400){
-			applyTheme(xhr.response)
-		}
+		xhr.onload = (e) => applyTheme(xhr.response)
+		xhr.onerror = (e) => console.log("Error: ", uri, xhr, e)
+
+		xhr.open("GET", uri)
+		xhr.send(null)
+	} else{
+		parseCustomTheme()
 	}
-	xhr.onerror = (e) => console.log("Error: ", uri, xhr, e);
-
-	xhr.open('GET', uri);
-	xhr.send(null);
-
-	// (async () => {
-	// 	localStorage.theme = JSON.stringify(cssObj);
-	// 	EDITOR.value = (() => {
-	// 		let styleString = '';
-	// 		for (let key in cssObj) styleString += `${key}: ${cssObj[key]}\n`;
-	// 		return styleString;
-	// 	})();
-    //
-	// 	return cssObj
-	// })().then(cssObj => {
-	// 	let css = ':root{';
-	// 	for (let key in cssObj) css += `--${key}:${cssObj[key]};`;
-	// 	css += '}';
-    //
-	// 	let node = sel('#theme');
-	// 	if (node) node.innerHTML = css
-	// 	else{
-	// 		createElement('style', {
-	// 			'type': 'text/css',
-	// 			'innerHTML': css,
-	// 			'id': 'theme'
-	// 		}).then(el => sel('head').appendChild(el))
-	// 	}
-	// })
 }
-
-// const EDITOR = document.querySelector('#themeEditor');
-//
-// EDITOR.onchange = async () => {
-// 	let cssObj = {};
-// 	for (str of EDITOR.value.split('\n')) {
-// 		if (str) {
-// 			let splited = str.split(': ');
-// 			cssObj[splited[0]] = splited[1];
-// 		}
-// 	};
-//
-// 	setTheme(cssObj);
-// };
 
 function applyTheme(json) {
 	localStorage.theme = json
 	let object = JSON.parse(json)
 
 	for (let rule in object) {
+		document.querySelector(`#themeEditor [name="${rule}"]`).value = object[rule]
 		document.documentElement.style.setProperty(`--${rule}`, object[rule])
 	}
+}
+
+function parseCustomTheme(){
+	let object = {}
+
+	for (let input of document.querySelectorAll("#themeEditor input")) {
+		object[input.name] = input.value
+	}
+
+	applyTheme(JSON.stringify(object))
 }
