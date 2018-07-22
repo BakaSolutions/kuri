ce614b75b75a06f7244f16e78e5b2d44201c318c
man90 <man90@tuta.io>
2018-07-22 18:54:12 +0200

    Implement captcha

--- src/scripts/posting.js
+++ src/scripts/posting.js
@@ -1,6 +1,17 @@
-function sendPost(e){ // TODO: Проверка валидности всех полей
+async function handlePostSend(e) { // TODO: Проверка валидности всех полей
 	e.preventDefault()
 
+	let c = await captcha.check()
+
+	if (c.trustedPostCount){
+		sendPost()
+	} else{
+		captcha.update()
+		toggleWidget("captcha")
+	}
+}
+
+function sendPost() {
 	let form = sel("#postForm"),
 		uri = form.action,
 		formData = new FormData(form),
@@ -36,4 +47,43 @@ function sendPost(e){ // TODO: Проверка валидности всех п
 				class: 'notification'
 			})
 		})
+}
+
+captcha = {
+	update: function() {
+		sel("#captcha img").src = "https://tuderi.tumba.ch:48596/api/v1/captcha.image" + "?" + +new Date()
+		sel("#captcha input").value = ""
+	},
+
+	check: async function() {
+		let uri = "https://tuderi.tumba.ch:48596/api/v1/captcha.check",
+			options = {
+				credentials: "include",
+				method: "post",
+				body: new FormData(sel("#captcha form"))
+			}
+
+		return fetch(uri, options)
+			.then(response => {
+				if (response.status == 200) {
+					return response.json()
+				} else {
+					throw response.status
+				}
+			})
+			.catch(err => console.error("Fetch error occured:", err))
+	},
+
+	send: async function() {
+		if (sel("#captcha input").value) {
+			let c = await this.check()
+
+			if (c.passed) {
+				toggleWidget("captcha")
+				sendPost()
+			} else{
+				this.update()
+			}
+		}
+	}
 }
