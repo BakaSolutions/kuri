99bcebcb50fe27dc0252c486e4dcc17210c0d353
man90 <man90@tuta.io>
2018-08-01 17:52:50 +0200

    Fix scrolling bug

--- config.js
+++ config.js
@@ -14,8 +14,10 @@ module.exports = {
     enable: false
   },
   foxtan: {
-    http: URL.parse('https://tuderi.tumba.ch:48596/'),
-    websocket: URL.parse('wss://tuderi.tumba.ch:48596/ws'),
+    // http: URL.parse('https://tuderi.tumba.ch:48596/'),
+    http: URL.parse('https://foxtan.tuderi.tumba.ch/'),
+    // websocket: URL.parse('wss://tuderi.tumba.ch:48596/ws'),
+    websocket: URL.parse('wss://foxtan.tuderi.tumba.ch/ws'),
   },
   server: {
     port: 8080
